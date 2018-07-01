71cbdd39c452f0daf21b3466e8ce84150f965276
Simeon Shterev <simeon_dionisovsh@abv.bg>
2018-07-01 21:40:13 +0300

    adding Speaker entity

---
+++ src/main/java/com/teamthree/conferencescheduler/entities/Speaker.java
@@ -0,0 +1,57 @@
+package com.teamthree.conferencescheduler.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "Speakers")
+public class Speaker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "photo")
+    private String photo;
+
+    public Speaker(){}
+
+    public Speaker(String name, String description, String photo){
+        this.name = name;
+        this.description = description;
+        this.photo = photo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
