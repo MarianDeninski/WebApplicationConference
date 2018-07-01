23ad3d2a1f25f29b940cfb05a5c7d58658aa3a80
Miroslav <miro.vd1@abv.bg>
2018-07-01 20:28:33 +0300

    Created Entity Halls.

---
+++ src/main/java/com/teamthree/conferencescheduler/entities/Hall.java
@@ -0,0 +1,68 @@
+package com.teamthree.conferencescheduler.entities;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "halls")
+public class Hall {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "seat_capacity")
+    private long seatCapacity;
+
+    @Transient
+    private Venue venue;
+
+    @Transient
+    private List<Session> sessions;
+
+    public Hall() {
+        this.sessions = new ArrayList<>();
+    }
+
+    public Hall(String name, long seatCapacity, Venue venue, List<Session> sessions) {
+        this.name = name;
+        this.seatCapacity = seatCapacity;
+        this.venue = venue;
+        this.sessions = sessions;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getSeatCapacity() {
+        return this.seatCapacity;
+    }
+
+    public void setSeatCapacity(long seatCapacity) {
+        this.seatCapacity = seatCapacity;
+    }
+
+    public Venue getVenue() {
+        return this.venue;
+    }
+
+    public void setVenue(Venue venue) {
+        this.venue = venue;
+    }
+
+    public List<Session> getSessions() {
+        return this.sessions;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
+    }
+}
