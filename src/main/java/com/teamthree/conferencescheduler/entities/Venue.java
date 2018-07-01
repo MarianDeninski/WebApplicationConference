bdf14eb7f954e1a35600418c1690b7d43978320d
Tihomir96 <tihomirkirilov96@gmail.com>
2018-07-01 21:51:21 +0300

    Revert "Merge branch 'master' of https://github.com/CodexioDevCamp-TeamIII/ConferenceScheduler"
    This reverts commit 0a42354d3aa8bf1fc5215fb2eed7257ed151700c, reversing
    changes made to 4e22440ef8a8576e544e4f4018f5812268c44c0e.

--- src/main/java/com/teamthree/conferencescheduler/entities/Venue.java
+++ src/main/java/com/teamthree/conferencescheduler/entities/Venue.java
@@ -5,15 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-<<<<<<< HEAD
 @Table(name="venues")
-=======
-@Table(name = "venues")
->>>>>>> 2ccc06989bd09739d896c5aa0c61caf3e3ae0585
 public class Venue {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-<<<<<<< HEAD
      private Long id;
 
     public Venue() {
@@ -25,36 +21,24 @@ public class Venue {
         this.address = address;
         this.halls = halls;
     }
-=======
-    private Long id;
->>>>>>> 2ccc06989bd09739d896c5aa0c61caf3e3ae0585
 
     @Column(name = "address")
     private String address;
 
-<<<<<<< HEAD
     @Column(name ="halls")
     @OneToMany(mappedBy = "hall")
-=======
-    @Transient
->>>>>>> 2ccc06989bd09739d896c5aa0c61caf3e3ae0585
     private List<Hall> halls;
 
-    public Venue() {
-        this.halls = new ArrayList<>();
+    public void setAddress(String address) {
+        this.address = address;
     }
 
-    public Venue(String address, List<Hall> halls) {
-        this.address = address;
+    public void setHalls(List<Hall> halls) {
         this.halls = halls;
     }
 
     public List<Hall> getHalls() {
         return this.halls;
-    }
-
-    public void setHalls(List<Hall> halls) {
-        this.halls = halls;
     }
 
     public Long getId() {
@@ -63,9 +47,5 @@ public class Venue {
 
     public String getAddress() {
         return this.address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 }
