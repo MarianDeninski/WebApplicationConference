1e03e96e7fea24f443d0b9614bd8ae44aae0f912
Tihomir96 <tihomirkirilov96@gmail.com>
2018-07-01 21:00:27 +0300

    added Venue entity
    added Venue entity

--- src/main/java/com/teamthree/conferencescheduler/entities/Venue.java
+++ src/main/java/com/teamthree/conferencescheduler/entities/Venue.java
@@ -1,4 +1,48 @@
 package com.teamthree.conferencescheduler.entities;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table
 public class Venue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
+
+    public Venue() {
+    }
+
+    public Venue(Long id, String address, List<Hall> halls) {
+        this.id = id;
+        this.address = address;
+        this.halls = halls;
+    }
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name ="halls")
+    private List<Hall> halls;
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setHalls(List<Hall> halls) {
+        this.halls = halls;
+    }
+
+    public List<Hall> getHalls() {
+        return halls;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
