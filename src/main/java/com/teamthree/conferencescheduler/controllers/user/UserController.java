1b8eeceac0250f3bf40253fd36b18c1cb12122f8
Trinity7509 <iren75iv@gmail.com>
2018-07-02 16:16:35 +0300

    Login/logout added
    Login/logout added

--- src/main/java/com/teamthree/conferencescheduler/controllers/user/UserController.java
+++ src/main/java/com/teamthree/conferencescheduler/controllers/user/UserController.java
@@ -6,11 +6,19 @@ import com.teamthree.conferencescheduler.entities.User;
 import com.teamthree.conferencescheduler.service.api.RoleService;
 import com.teamthree.conferencescheduler.service.api.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import static com.teamthree.conferencescheduler.constants.roadsMappings.RoadMapping.USER_LOGIN;
 import static com.teamthree.conferencescheduler.constants.roadsMappings.RoadMapping.USER_REGISTER;
@@ -60,5 +68,15 @@ public class UserController {
         this.userService.createNewUser(user);
 
         return "redirect:/login";
+    }
+
+    @RequestMapping(value="/logout",method = RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
     }
 }
