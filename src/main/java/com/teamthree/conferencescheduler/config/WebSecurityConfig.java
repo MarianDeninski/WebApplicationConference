35d934ac936afb07e65c4ab45dbfa3bab55604ff
Miroslav <miro.vd1@abv.bg>
2018-07-03 03:19:55 +0300

    implemented error handling page.

--- src/main/java/com/teamthree/conferencescheduler/config/WebSecurityConfig.java
+++ src/main/java/com/teamthree/conferencescheduler/config/WebSecurityConfig.java
@@ -59,7 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout().logoutSuccessUrl("/user/login?logout")
                 .and()
-                .exceptionHandling().accessDeniedPage("/error/403")
+                .exceptionHandling().accessDeniedPage("/err/403")
                 .and()
                 .csrf();
     }
---
+++ src/main/java/com/teamthree/conferencescheduler/controllers/error/ApplicationErrorController.java
@@ -0,0 +1,16 @@
+package com.teamthree.conferencescheduler.controllers.error;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ApplicationErrorController implements ErrorController {
+
+    @Override
+    @RequestMapping(value = "/error", method = RequestMethod.GET)
+    public String getErrorPath() {
+        return "error-layout";
+    }
+}
