f9634e71c21cbee37d40584d1ecd8ed6017bdf2a
Miroslav <miro.vd1@abv.bg>
2018-07-04 00:58:16 +0300

    changed AddVenueDto - new field added name to match the view form.

--- src/main/java/com/teamthree/conferencescheduler/dto/venue/AddVenueDto.java
+++ src/main/java/com/teamthree/conferencescheduler/dto/venue/AddVenueDto.java
@@ -6,13 +6,32 @@ import java.io.Serializable;
 public class AddVenueDto implements Serializable {
 
     @NotNull
+    private String name;
+
+    @NotNull
     private String address;
 
-    public AddVenueDto(String address){
-        this.address=address;
+    public AddVenueDto() {
+    }
+
+    public AddVenueDto(String name, String address) {
+        this.name = name;
+        this.address = address;
     }
 
     public String getAddress() {
         return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
