aa28f85f69f9c2f21fab3c65013e7bd761b091d2
Miroslav <miro.vd1@abv.bg>
2018-07-04 02:39:44 +0300

    merge

--- src/main/java/com/teamthree/conferencescheduler/app_utils/UserUtil.java
+++ src/main/java/com/teamthree/conferencescheduler/app_utils/UserUtil.java
@@ -4,7 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class UserUtil {
-    public Authentication getLoggedUser() {
+    public static Authentication getLoggedUser() {
         return SecurityContextHolder.getContext().getAuthentication();
     }
 }
--- src/main/java/com/teamthree/conferencescheduler/controllers/venue/VenueController.java
+++ src/main/java/com/teamthree/conferencescheduler/controllers/venue/VenueController.java
@@ -1,18 +1,23 @@
 package com.teamthree.conferencescheduler.controllers.venue;
 
 import com.sun.org.apache.xpath.internal.operations.Mod;
+import com.teamthree.conferencescheduler.app_utils.UserUtil;
 import com.teamthree.conferencescheduler.dto.venue.AddVenueDto;
+import com.teamthree.conferencescheduler.entities.User;
 import com.teamthree.conferencescheduler.entities.Venue;
 import com.teamthree.conferencescheduler.exceptions.ApplicationRuntimeException;
 import com.teamthree.conferencescheduler.repositories.VenueRepository;
 import com.teamthree.conferencescheduler.service.api.VenueService;
 import com.teamthree.conferencescheduler.service.impl.VenueServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 import static com.teamthree.conferencescheduler.constants.roadsMappings.RoadMapping.ADD_VENUE;
 import static com.teamthree.conferencescheduler.constants.views.ViewConstants.*;
@@ -42,7 +47,13 @@ public class VenueController {
     public String processVenue(Model model, AddVenueDto dto) {
 
 
-        Venue venue = new Venue(dto.getAddress(), dto.getName());
+        Principal principal = UserUtil.getLoggedUser();
+        String name = principal.getName();
+        Object details = ((Authentication) principal).getDetails();
+        Object principal1 = ((Authentication) principal).getPrincipal();
+
+
+        Venue venue = new Venue(dto.getAddress(), dto.getName(), new User());
         try {
             this.venueService.addVenue(venue);
         } catch (ApplicationRuntimeException ex) {
