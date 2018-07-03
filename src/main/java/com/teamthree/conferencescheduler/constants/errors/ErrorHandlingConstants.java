ee57ff2188f862d2c6aa0a5edbd9873cda76c4af
skorae <skorae135@gmail.com>
2018-07-03 20:25:42 +0300

    conferenceHtml

--- src/main/java/com/teamthree/conferencescheduler/constants/errors/ErrorHandlingConstants.java
+++ src/main/java/com/teamthree/conferencescheduler/constants/errors/ErrorHandlingConstants.java
@@ -1,7 +1,7 @@
 package com.teamthree.conferencescheduler.constants.errors;
 
 public final class ErrorHandlingConstants {
-    public static final String EMAIL_EXISTS = "Sorry, this email already exists. Please try new one!";
+    public static final String EMAIL_EXISTS = "Sorry, this username already exists. Please try new one!";
     public static final String EMAIL_FIELD_IS_EMPTY = "Username field, must not be empty!";
     public static final String NAME_FIELD_IS_EMPTY = "Name field, must not be empty!";
     public static final String PASSWORD_FIELD_IS_EMPTY = "Password field, must not be empty!";
