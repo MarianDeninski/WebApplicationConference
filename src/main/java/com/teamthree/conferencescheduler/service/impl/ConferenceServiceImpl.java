9b75fa80140075e096aec5a27d0a39d0e97664a2
Tihomir96 <tihomirkirilov96@gmail.com>
2018-07-04 00:35:26 +0300

    ConferenceService

---
+++ src/main/java/com/teamthree/conferencescheduler/service/impl/ConferenceServiceImpl.java
@@ -0,0 +1,23 @@
+package com.teamthree.conferencescheduler.service.impl;
+
+import com.teamthree.conferencescheduler.entities.Conference;
+import com.teamthree.conferencescheduler.repositories.ConferenceRepository;
+import com.teamthree.conferencescheduler.service.api.ConferenceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConferenceServiceImpl implements ConferenceService {
+
+    private ConferenceRepository conferenceRepository;
+
+    @Autowired
+    public ConferenceServiceImpl(ConferenceRepository conferenceRepository){
+        this.conferenceRepository = conferenceRepository;
+    }
+
+    public void createNewConference(Conference conference){
+        this.conferenceRepository.saveAndFlush(conference);
+    }
+
+}
