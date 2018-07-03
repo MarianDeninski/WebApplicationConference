86c09df70a5a0cb2462bb7ef394ccea6eecaf4dd
Miroslav <miro.vd1@abv.bg>
2018-07-04 00:30:05 +0300

    initial commit on venue view

--- src/main/java/com/teamthree/conferencescheduler/constants/roadsMappings/RoadMapping.java
+++ src/main/java/com/teamthree/conferencescheduler/constants/roadsMappings/RoadMapping.java
@@ -4,6 +4,7 @@ public final class RoadMapping {
     public static final String HOME_INDEX = "home/index";
     public static final String USER_REGISTER = "user/register";
     public static final String USER_LOGIN = "user/login";
+    public static final String ADD_VENUE = "venue/add_venue";
 
     private RoadMapping() {
     }
--- src/main/java/com/teamthree/conferencescheduler/controllers/venue/VenueController.java
+++ src/main/java/com/teamthree/conferencescheduler/controllers/venue/VenueController.java
@@ -3,9 +3,14 @@ package com.teamthree.conferencescheduler.controllers.venue;
 import com.teamthree.conferencescheduler.repositories.VenueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import static com.teamthree.conferencescheduler.constants.roadsMappings.RoadMapping.ADD_VENUE;
+import static com.teamthree.conferencescheduler.constants.views.ViewConstants.BASE_LAYOUT;
+import static com.teamthree.conferencescheduler.constants.views.ViewConstants.VIEW;
 
 @Controller
 @RequestMapping("/venue")
@@ -14,14 +19,16 @@ public class VenueController {
     private VenueRepository venueRepository;
 
     @Autowired
-    public VenueController(VenueRepository venueRepository){
-        this.venueRepository=venueRepository;
+    public VenueController(VenueRepository venueRepository) {
+        this.venueRepository = venueRepository;
     }
 
 
     @GetMapping("/add")
-    public String addVenue(){
+    //TODO: IT SHOULD NOT BE POSSIBLE TO ACCESS THIS PAGE WITHOUT LOGIN!
+    public String addVenue(Model model) {
 
-    return null;
+        model.addAttribute(VIEW, ADD_VENUE);
+        return BASE_LAYOUT;
     }
 }
