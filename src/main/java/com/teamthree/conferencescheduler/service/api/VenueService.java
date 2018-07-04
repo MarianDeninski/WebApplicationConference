8a3b4f32cf0e8feb8558ddd2111f0b35790b4ed2
Miroslav <miro.vd1@abv.bg>
2018-07-04 18:14:15 +0300

    added new view add hall.

--- src/main/java/com/teamthree/conferencescheduler/service/api/VenueService.java
+++ src/main/java/com/teamthree/conferencescheduler/service/api/VenueService.java
@@ -2,8 +2,12 @@ package com.teamthree.conferencescheduler.service.api;
 
 import com.teamthree.conferencescheduler.entities.Venue;
 
+import java.util.List;
+
 public interface VenueService {
     void addVenue(Venue venue);
 
     Venue getVenueById(long id);
+
+    List<Venue> getAllVenues();
 }
