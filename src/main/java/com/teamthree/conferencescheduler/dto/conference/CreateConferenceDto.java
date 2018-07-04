26e14832d21e283951fcf41cd3b6fb8371189ef0
Tihomir96 <tihomirkirilov96@gmail.com>
2018-07-04 14:52:33 +0300

    create conference dto and html

--- src/main/java/com/teamthree/conferencescheduler/dto/conference/CreateConferenceDto.java
+++ src/main/java/com/teamthree/conferencescheduler/dto/conference/CreateConferenceDto.java
@@ -26,32 +26,52 @@ public class CreateConferenceDto implements Serializable {
 
     }
 
-    public CreateConferenceDto(String name,String description,String venueName,String startDate,String endDate){
-
-        this.name=name;
-        this.description=description;
-        this.venueName=venueName;
-        this.startDate=startDate;
-        this.endDate=endDate;
-    }
-
-    public String getVenueName() {
-        return venueName;
-    }
-
     public String getName() {
         return name;
     }
 
-    public String getEndDate() {
-        return endDate;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getStartDate() {
         return startDate;
     }
 
-    public String getDescription() {
-        return description;
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getVenueName() {
+        return venueName;
+    }
+
+    public void setVenueName(String venueName) {
+        this.venueName = venueName;
+    }
+
+    public CreateConferenceDto(String name, String description, String startDate, String endDate, String venueName) {
+
+        this.name = name;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.venueName = venueName;
     }
 }
