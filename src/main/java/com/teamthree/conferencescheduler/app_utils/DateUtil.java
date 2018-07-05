e4c9b77340223cb3c82b263fe7445ebddf8784eb
Miroslav <miro.vd1@abv.bg>
2018-07-05 15:15:07 +0300

    user cant add end date before start date in conf creation fix

--- src/main/java/com/teamthree/conferencescheduler/app_utils/DateUtil.java
+++ src/main/java/com/teamthree/conferencescheduler/app_utils/DateUtil.java
@@ -11,8 +11,8 @@ public class DateUtil {
     }
 
     public static boolean checkIfPeriodIsValid(String startDate, String endDate) {
-        int[] startPeriod = Arrays.stream(startDate.split("/")).mapToInt(Integer::parseInt).toArray();
-        int[] endPeriod = Arrays.stream(endDate.split("/")).mapToInt(Integer::parseInt).toArray();
+        int[] startPeriod = Arrays.stream(startDate.split("-")).mapToInt(Integer::parseInt).toArray();
+        int[] endPeriod = Arrays.stream(endDate.split("-")).mapToInt(Integer::parseInt).toArray();
 
         if (startPeriod[0] > endPeriod[0] || startPeriod[1] > endPeriod[1] || startPeriod[2] > endPeriod[2]) {
             throw new ApplicationRuntimeException("Start date must be before End date!");
